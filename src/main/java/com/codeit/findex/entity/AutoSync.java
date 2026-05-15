dc72dadd8f916b084752789fd40caf733abcfc5f
99hyeon <hyeon99@ajou.ac.kr>
2026-05-15 18:11:36 +0900

    setting: base setting

---
+++ src/main/java/com/codeit/findex/entity/AutoSync.java
@@ -0,0 +1,37 @@
+package com.codeit.findex.entity;
+
+import com.codeit.findex.entity.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@Table(name = "auto_sync")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AutoSync extends BaseEntity {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "index_info_id", nullable = false, unique = true)
+    private IndexInfo indexInfo;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+    public AutoSync(IndexInfo indexInfo, Boolean enabled) {
+        super();
+        this.indexInfo = indexInfo;
+        this.enabled = enabled;
+    }
+
+}
