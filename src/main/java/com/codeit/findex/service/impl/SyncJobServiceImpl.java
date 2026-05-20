4d8a60dd3093fb06d0dbc202e7283be9965005c6
99hyeon <hyeon99@ajou.ac.kr>
2026-05-20 14:24:52 +0900

    [#19] refactor: api 연동 시 autoSync 저장 로직 추가

--- src/main/java/com/codeit/findex/service/impl/SyncJobServiceImpl.java
+++ src/main/java/com/codeit/findex/service/impl/SyncJobServiceImpl.java
@@ -11,6 +11,7 @@ import com.codeit.findex.entity.JobType;
 import com.codeit.findex.entity.Result;
 import com.codeit.findex.entity.SourceType;
 import com.codeit.findex.entity.SyncJob;
+import com.codeit.findex.repository.AutoSyncRepository;
 import com.codeit.findex.repository.SyncJobRepository;
 import com.codeit.findex.service.ClientIndexSyncService;
 import com.codeit.findex.service.SyncJobService;
