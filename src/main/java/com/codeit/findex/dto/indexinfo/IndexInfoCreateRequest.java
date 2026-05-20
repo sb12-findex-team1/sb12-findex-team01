c684591054cf3a2424bd95d3dd4edba0d136fd61
yesungyoo <yeseong.yys@gmail.com>
2026-05-20 12:20:47 +0900

    refactor: update null 체크 및 CreateRequest 검증 어노테이션 추가

--- src/main/java/com/codeit/findex/dto/indexinfo/IndexInfoCreateRequest.java
+++ src/main/java/com/codeit/findex/dto/indexinfo/IndexInfoCreateRequest.java
@@ -1,14 +1,16 @@
 package com.codeit.findex.dto.indexinfo;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record IndexInfoCreateRequest(
 
-    String indexName,            // 지수명
-    String indexClassification,  // 지수 분류명
-    Integer employedItemsCount,  // 채용 종목 수
-    BigDecimal baseIndex,        // 기준 지수
-    LocalDate basePointInTime,     // 기준 시점
+    @NotBlank String indexName,            // 지수명
+    @NotBlank String indexClassification,  // 지수 분류명
+    @NotNull Integer employedItemsCount,  // 채용 종목 수
+    @NotNull BigDecimal baseIndex,        // 기준 지수
+    @NotNull LocalDate basePointInTime,     // 기준 시점
     boolean favorite             // 즐겨찾기
 ) {}
