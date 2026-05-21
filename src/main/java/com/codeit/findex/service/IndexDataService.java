a0da20571eb3a623440a71c817a4380e84a56a88
Seungji Lyoo <lyoonatelie@gmail.com>
2026-05-21 10:17:31 +0900

    feat/ 오류건 queryDSL추가1

--- src/main/java/com/codeit/findex/service/IndexDataService.java
+++ src/main/java/com/codeit/findex/service/IndexDataService.java
@@ -10,8 +10,9 @@ import com.codeit.findex.dto.indexdata.RankedIndexPerformanceDto;
 import com.codeit.findex.entity.IndexData;
 import com.codeit.findex.entity.PeriodType;
 import java.util.List;
-import java.util.Optional;
+import org.springframework.data.domain.Page;
 import java.util.UUID;
+
 
 
 public interface IndexDataService {
@@ -19,7 +20,7 @@ public interface IndexDataService {
   IndexDataResponse create(IndexDataCreateRequest request);
   IndexDataResponse update(UUID id, IndexDataUpdateRequest request);
   void delete(UUID id);
-  Optional search(IndexDataSearchRequest request);
+  Page<IndexDataResponse> search(IndexDataSearchRequest request);
   List<IndexData> findAllForExport(IndexDataSearchRequest request);
 
   IndexChartDto getChartData(UUID id, PeriodType periodType);
